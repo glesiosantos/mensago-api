9e0f1d5c4363169ea6360d387236c231182f87d6
Glêsio Santos <glesioss@gmail.com>
2026-09-15 21:50:47 -0300

    criando o construct de campaign

--- internal/domain/campaign/model.go
+++ internal/domain/campaign/model.go
@@ -1,8 +1,10 @@
 package campaign
 
-import "time"
+import (
+	"time"
+)
 
-type Contacts struct {
+type Contact struct {
 	Email string
 }
 
@@ -10,6 +12,23 @@ type Campaign struct {
 	Id        string
 	Name      string
 	Content   string
-	Contacts  []Contacts
+	Contacts  []Contact
 	CreatedAt time.Time
+}
+
+func NewCampaign(name string, content string, emails []string) *Campaign {
+
+	contacts := make([]Contact, len(emails))
+
+	for index, email := range emails {
+		contacts[index].Email = email
+	}
+
+	return &Campaign{
+		Id:        "1",
+		Name:      name,
+		Content:   content,
+		Contacts:  contacts,
+		CreatedAt: time.Now(),
+	}
 }
